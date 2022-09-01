f11b8156c1e7c5ab543e701f5be13368d0f1837f
VladimirPirozhenko <vvpirozhok@mail.ru>
2022-09-01 23:42:31 +0300

    Initial commit 2nd task
    Movement + IInteractable

---
+++ MovingTurtles/Source/MovingTurtles/MovingTurtlesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MovingTurtlesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOVINGTURTLES_API AMovingTurtlesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
